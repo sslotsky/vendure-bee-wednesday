23e686acacc5d80c6dc20c4b49967540c1669a00
Sam Slotsky <saxosamo@gmail.com>
2023-04-25 12:28:05 -0500

    Refactor vendure config

--- migration.ts
+++ migration.ts
@@ -1,27 +1,34 @@
-import { generateMigration, revertLastMigration, runMigrations } from '@vendure/core';
-import program from 'commander';
+import {
+  generateMigration,
+  revertLastMigration,
+  runMigrations,
+} from "@vendure/core";
+import program from "commander";
 
-import { config } from './src/vendure-config';
-
-program
-    .command('generate <name>')
-    .description('Generate a new migration file with the given name')
-    .action(name => {
-        return generateMigration(config, { name, outputDir: './src/migrations' });
-    });
+import { baseConfig } from "./src/vendure-config";
 
 program
-    .command('run')
-    .description('Run all pending migrations')
-    .action(() => {
-        return runMigrations(config);
+  .command("generate <name>")
+  .description("Generate a new migration file with the given name")
+  .action((name) => {
+    return generateMigration(baseConfig, {
+      name,
+      outputDir: "./src/migrations",
     });
+  });
 
 program
-    .command('revert')
-    .description('Revert the last applied migration')
-    .action(() => {
-        return revertLastMigration(config);
-    });
+  .command("run")
+  .description("Run all pending migrations")
+  .action(() => {
+    return runMigrations(baseConfig);
+  });
+
+program
+  .command("revert")
+  .description("Revert the last applied migration")
+  .action(() => {
+    return revertLastMigration(baseConfig);
+  });
 
 program.parse(process.argv);
--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -31,103 +31,107 @@ const printsPreparedHandler = new EmailEventListener("test-email")
   .setSubject("This is a test")
   .setTemplateVars((evt) => ({ payload: evt.payload }));
 
+export const baseConfig: VendureConfig = {
+  taxOptions: {
+    taxZoneStrategy: new DefaultTaxZoneStrategy(),
+    taxLineCalculationStrategy: new DefaultTaxLineCalculationStrategy(),
+  },
+  apiOptions: {
+    port: 8080,
+    adminApiPath: "admin-api",
+    shopApiPath: "shop-api",
+    // The following options are useful in development mode,
+    // but are best turned off for production for security
+    // reasons.
+    ...(IS_DEV
+      ? {
+          adminApiPlayground: {
+            settings: { "request.credentials": "include" } as any,
+          },
+          adminApiDebug: true,
+          shopApiPlayground: {
+            settings: { "request.credentials": "include" } as any,
+          },
+          shopApiDebug: true,
+        }
+      : {}),
+  },
+  authOptions: {
+    tokenMethod: ["bearer", "cookie"],
+    superadminCredentials: {
+      identifier: process.env.SUPERADMIN_USERNAME,
+      password: process.env.SUPERADMIN_PASSWORD,
+    },
+    cookieOptions: {
+      secret: process.env.COOKIE_SECRET,
+    },
+  },
+  dbConnectionOptions: IS_DEV
+    ? {
+        type: "postgres",
+        // See the README.md "Migrations" section for an explanation of
+        // the `synchronize` and `migrations` options.
+        migrations: [path.join(__dirname, "./migrations/*.+(js|ts)")],
+        logging: false,
+        database: process.env.DB_NAME,
+        schema: process.env.DB_SCHEMA,
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+      }
+    : {
+        type: "postgres",
+        // See the README.md "Migrations" section for an explanation of
+        // the `synchronize` and `migrations` options.
+        synchronize: false,
+        migrations: [path.join(__dirname, "./migrations/*.+(js|ts)")],
+        logging: false,
+        url: process.env.DATABASE_URL,
+      },
+  paymentOptions: {
+    paymentMethodHandlers: [dummyPaymentHandler],
+  },
+  // When adding or altering custom field definitions, the database will
+  // need to be updated. See the "Migrations" section in README.md.
+  customFields: {
+    ProductVariant: [
+      {
+        name: "height",
+        type: "int",
+      },
+      {
+        name: "width",
+        type: "int",
+      },
+      {
+        name: "material",
+        type: "string",
+      },
+    ],
+    OrderLine: [
+      {
+        name: "transformation",
+        type: "string",
+        validate: async (value: string) => {
+          const pattern =
+            /^tr:w-\d+(.\d+),h-\d+(.\d+),cm-extract,x-\d+(.\d+),y-\d+(.\d+)?$/;
+
+          if (!pattern.test(value)) {
+            return "Transformation not valid";
+          }
+        },
+      },
+      { name: "fileId", type: "string" },
+    ],
+  },
+};
+
 export async function getConfig(): Promise<VendureConfig> {
   const customUi = await customAdminUi({ recompile: IS_DEV, devMode: IS_DEV });
 
   return {
-    taxOptions: {
-      taxZoneStrategy: new DefaultTaxZoneStrategy(),
-      taxLineCalculationStrategy: new DefaultTaxLineCalculationStrategy(),
-    },
-    apiOptions: {
-      port: 8080,
-      adminApiPath: "admin-api",
-      shopApiPath: "shop-api",
-      // The following options are useful in development mode,
-      // but are best turned off for production for security
-      // reasons.
-      ...(IS_DEV
-        ? {
-            adminApiPlayground: {
-              settings: { "request.credentials": "include" } as any,
-            },
-            adminApiDebug: true,
-            shopApiPlayground: {
-              settings: { "request.credentials": "include" } as any,
-            },
-            shopApiDebug: true,
-          }
-        : {}),
-    },
-    authOptions: {
-      tokenMethod: ["bearer", "cookie"],
-      superadminCredentials: {
-        identifier: process.env.SUPERADMIN_USERNAME,
-        password: process.env.SUPERADMIN_PASSWORD,
-      },
-      cookieOptions: {
-        secret: process.env.COOKIE_SECRET,
-      },
-    },
-    dbConnectionOptions: IS_DEV
-      ? {
-          type: "postgres",
-          // See the README.md "Migrations" section for an explanation of
-          // the `synchronize` and `migrations` options.
-          migrations: [path.join(__dirname, "./migrations/*.+(js|ts)")],
-          logging: false,
-          database: process.env.DB_NAME,
-          schema: process.env.DB_SCHEMA,
-          host: process.env.DB_HOST,
-          port: +process.env.DB_PORT,
-          username: process.env.DB_USERNAME,
-          password: process.env.DB_PASSWORD,
-        }
-      : {
-          type: "postgres",
-          // See the README.md "Migrations" section for an explanation of
-          // the `synchronize` and `migrations` options.
-          synchronize: false,
-          migrations: [path.join(__dirname, "./migrations/*.+(js|ts)")],
-          logging: false,
-          url: process.env.DATABASE_URL,
-        },
-    paymentOptions: {
-      paymentMethodHandlers: [dummyPaymentHandler],
-    },
-    // When adding or altering custom field definitions, the database will
-    // need to be updated. See the "Migrations" section in README.md.
-    customFields: {
-      ProductVariant: [
-        {
-          name: "height",
-          type: "int",
-        },
-        {
-          name: "width",
-          type: "int",
-        },
-        {
-          name: "material",
-          type: "string",
-        },
-      ],
-      OrderLine: [
-        {
-          name: "transformation",
-          type: "string",
-          validate: async (value: string) => {
-            const pattern =
-              /^tr:w-\d+(.\d+),h-\d+(.\d+),cm-extract,x-\d+(.\d+),y-\d+(.\d+)?$/;
-
-            if (!pattern.test(value)) {
-              return "Transformation not valid";
-            }
-          },
-        },
-        { name: "fileId", type: "string" },
-      ],
-    },
+    ...baseConfig,
     plugins: [
       PrintPreparerPlugin,
       StripePlugin.init({
